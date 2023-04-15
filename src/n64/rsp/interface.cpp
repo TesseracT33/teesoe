9641b648e6f9c6faceafa23c2db7718488ee325a
Christoffer Ekgrim <christoffer.ekgrim98@hotmail.com>
2023-04-15 14:03:58 +0200

    [N64] add IMEM/DMEM wrapping behaviour for dma, sp addr

--- src/n64/rsp/interface.cpp
+++ src/n64/rsp/interface.cpp
@@ -33,29 +33,23 @@ enum Register {
     Semaphore
 };
 
-static bool dma_in_progress, dma_is_pending;
 /* What was written last to either SP_DMA_RDLEN/ SP_DMA_WRLEN during an ongoing DMA */
-static s32 buffered_dma_rdlen, buffered_dma_wrlen, dma_spaddr_last_addr, dma_ramaddr_last_addr;
-static DmaType in_progress_dma_type;
-static void (*init_pending_dma_fun_ptr)();
+static s32 buffered_dma_rdlen, buffered_dma_wrlen;
+static DmaType in_progress_dma_type, pending_dma_type;
 
 constexpr s32 sp_pc_addr = 0x0408'0000;
 
-template<DmaType dma_type> static void InitDMA();
+template<DmaType dma_type> static void InitDma();
 static void OnDmaFinish();
 constexpr std::string_view RegOffsetToStr(u32 reg_offset);
 
-template<DmaType dma_type> void InitDMA()
+template<DmaType dma_type> void InitDma()
 {
-    static_assert(dma_type == DmaType::RdToSp || dma_type == DmaType::SpToRd);
-
-    dma_in_progress = true;
     in_progress_dma_type = dma_type;
+    sp.status.dma_busy = 1;
     sp.dma_busy |= 1;
-    sp.status.dma_busy = 1; /* Mirrors dma_busy.0 */
 
     s32 rows, bytes_per_row, skip;
-    u8 *src_ptr, *dst_ptr;
     if constexpr (dma_type == DmaType::RdToSp) {
         /* The number of bytes is RDLEN plus 1, rounded up to 8 bytes.
         Since the lower three bits of RDLEN are always 0, this effectively results
@@ -63,60 +57,17 @@ template<DmaType dma_type> void InitDMA()
         bytes_per_row = (sp.dma_rdlen & 0xFF8) + 8;
         rows = (sp.dma_rdlen >> 12 & 0xFF) + 1;
         skip = sp.dma_rdlen >> 20 & 0xFFF;
-        src_ptr = rdram::GetPointerToMemory(sp.dma_ramaddr);
-        dst_ptr = rsp::GetPointerToMemory(sp.dma_spaddr);
     } else {
         bytes_per_row = (sp.dma_wrlen & 0xFF8) + 8;
         rows = (sp.dma_wrlen >> 12 & 0xFF) + 1;
         skip = sp.dma_wrlen >> 20 & 0xFFF;
-        src_ptr = rsp::GetPointerToMemory(sp.dma_spaddr);
-        dst_ptr = rdram::GetPointerToMemory(sp.dma_ramaddr);
     }
 
-    s32 num_bytes_until_rdram_end = rdram::GetNumberOfBytesUntilMemoryEnd(sp.dma_ramaddr);
-    s32 num_bytes_until_spram_end = 0x1000 - (sp.dma_spaddr & 0xFFF);
-
-    s32 requested_total_bytes = rows * bytes_per_row;
-    s32 bytes_to_copy = std::min(requested_total_bytes, std::min(num_bytes_until_rdram_end, num_bytes_until_spram_end));
-    dma_spaddr_last_addr = sp.dma_spaddr + bytes_to_copy;
-    dma_ramaddr_last_addr = sp.dma_ramaddr + bytes_to_copy;
+    s32 bytes_to_copy = rows * bytes_per_row;
 
     /* The speed of transfer is about 3.7 bytes per VR4300 (PClock) cycle (plus some small fixed overhead). */
     static constexpr uint cpu_cycles_per_byte = 4;
-    uint cpu_cycles_until_finish = cpu_cycles_per_byte * requested_total_bytes;
-
-    scheduler::AddEvent(scheduler::EventType::SpDmaFinish, cpu_cycles_until_finish, OnDmaFinish);
-
-    /* The DMA engine allows to transfer multiple "rows" of data in RDRAM, separated by a "skip" value. This allows for
-    instance to transfer a rectangular portion of a larger image, by specifying the size of each row of the selection
-    portion, the number of rows, and a "skip" value that corresponds to the bytes between the end of a row and the
-    beginning of the following one. Notice that this applies only to RDRAM: accesses in IMEM/DMEM are always linear. */
-    if (skip == 0) {
-        for (size_t i = 0; i < bytes_to_copy; i += 4) {
-            s32 val;
-            std::memcpy(&val, src_ptr + i, 4);
-            val = std::byteswap(val); // rdram LE, rsp ram BE
-            std::memcpy(dst_ptr + i, &val, 4);
-        }
-    } else {
-        for (s32 i = 0; i < rows; ++i) {
-            s32 bytes_to_copy_this_row = std::min(bytes_to_copy, bytes_per_row);
-            for (s32 i = 0; i < bytes_to_copy_this_row; i += 4) {
-                s32 val;
-                std::memcpy(&val, src_ptr, 4);
-                val = std::byteswap(val); // rdram LE, rsp ram BE
-                std::memcpy(dst_ptr, &val, 4);
-                src_ptr += 4;
-                dst_ptr += 4;
-            }
-            bytes_to_copy -= bytes_to_copy_this_row;
-            if constexpr (dma_type == DmaType::RdToSp) {
-                src_ptr += skip;
-            } else {
-                dst_ptr += skip;
-            }
-        }
-    }
+    uint cpu_cycles_until_finish = cpu_cycles_per_byte * bytes_to_copy;
 
     if constexpr (log_dma) {
         std::string_view rsp_mem_bank = sp.dma_spaddr & 0x1000 ? "IMEM" : "DMEM";
@@ -126,27 +77,77 @@ template<DmaType dma_type> void InitDMA()
                   sp.dma_ramaddr & 0xFF'FFFF,
                   rsp_mem_bank,
                   sp.dma_spaddr & 0xFFF,
-                  requested_total_bytes);
+                  bytes_to_copy);
             } else {
                 return std::format("DMA; From RSP {} ${:X} to RDRAM ${:X}; ${:X} bytes",
                   rsp_mem_bank,
                   sp.dma_spaddr & 0xFFF,
                   sp.dma_ramaddr & 0xFF'FFFF,
-                  requested_total_bytes);
+                  bytes_to_copy);
             }
         }();
         log(output);
+    }
+
+    scheduler::AddEvent(scheduler::EventType::SpDmaFinish, cpu_cycles_until_finish, OnDmaFinish);
+
+    auto AlignAddresses = [] {
+        sp.dma_spaddr = sp.dma_spaddr & 0x1000 | sp.dma_spaddr + 4 & 0xFFF;
+        sp.dma_ramaddr += 4; // TODO: ensure no overflow
+    };
+
+    auto DstPtr = [] {
+        if constexpr (dma_type == DmaType::RdToSp) {
+            return rsp::GetPointerToMemory(sp.dma_spaddr);
+
+        } else {
+            return rdram::GetPointerToMemory(sp.dma_ramaddr);
+        }
+    };
+
+    auto SrcPtr = [] {
+        if constexpr (dma_type == DmaType::RdToSp) {
+            return rdram::GetPointerToMemory(sp.dma_ramaddr);
+        } else {
+            return rsp::GetPointerToMemory(sp.dma_spaddr);
+        }
+    };
+
+    /* The DMA engine allows to transfer multiple "rows" of data in RDRAM, separated by a "skip" value. This allows for
+       instance to transfer a rectangular portion of a larger image, by specifying the size of each row of the
+       selection portion, the number of rows, and a "skip" value that corresponds to the bytes between the end of a row
+       and the beginning of the following one. Notice that this applies only to RDRAM: accesses in IMEM/DMEM are always
+       linear. Moreover, if spaddr overflow it will wrap around within IMEM/DMEM (e.g. $FFC => 0; $1FFC => $1000)
+     */
+    if (skip == 0) {
+        for (size_t i = 0; i < bytes_to_copy; i += 4) {
+            s32 val;
+            std::memcpy(&val, SrcPtr(), 4);
+            val = std::byteswap(val); // rdram LE, rsp ram BE
+            std::memcpy(DstPtr(), &val, 4);
+            AlignAddresses();
+        }
+    } else {
+        for (s32 i = 0; i < rows; ++i) {
+            s32 bytes_to_copy_this_row = std::min(bytes_to_copy, bytes_per_row);
+            for (s32 i = 0; i < bytes_to_copy_this_row; i += 4) {
+                s32 val;
+                std::memcpy(&val, SrcPtr(), 4);
+                val = std::byteswap(val); // rdram LE, rsp ram BE
+                std::memcpy(DstPtr(), &val, 4);
+                AlignAddresses();
+            }
+            bytes_to_copy -= bytes_to_copy_this_row;
+            sp.dma_ramaddr += skip; // TODO: ensure no overflow
+        }
     }
 }
 
 void OnDmaFinish()
 {
-    sp.dma_spaddr = dma_spaddr_last_addr;
-    sp.dma_ramaddr = dma_ramaddr_last_addr;
-    if (dma_is_pending) {
-        dma_is_pending = false;
+    if (sp.status.dma_full) {
+        sp.status.dma_full = 0;
         sp.dma_full &= ~1;
-        sp.status.dma_full = 0; /* Mirrors dma_full.0 */
         if (in_progress_dma_type == DmaType::RdToSp) {
             sp.dma_rdlen = buffered_dma_rdlen;
             sp.dma_wrlen = buffered_dma_rdlen;
@@ -154,11 +155,14 @@ void OnDmaFinish()
             sp.dma_rdlen = buffered_dma_wrlen;
             sp.dma_wrlen = buffered_dma_wrlen;
         }
-        init_pending_dma_fun_ptr();
+        if (pending_dma_type == DmaType::RdToSp) {
+            InitDma<DmaType::RdToSp>();
+        } else {
+            InitDma<DmaType::SpToRd>();
+        }
     } else {
-        dma_in_progress = false;
+        sp.status.dma_busy = 0;
         sp.dma_busy &= ~1;
-        sp.status.dma_busy = 0; /* Mirrors dma_busy.0 */
         /* After the transfer is finished, the fields RDLEN and WRLEN contains the value 0xFF8,
         COUNT is reset to 0, and SKIP is unchanged. SPADDR and RAMADDR contain the
         addresses after the last ones that were read/written during the last DMA. */
@@ -187,7 +191,7 @@ u32 ReadReg(u32 addr)
         static_assert(sizeof(sp) >> 2 == 8);
         u32 offset = addr >> 2 & 7;
         u32 ret = [&] {
-            switch (offset & 7) {
+            switch (offset) {
             case DmaSpaddr: return sp.dma_spaddr;
 
             case DmaRamaddr: return sp.dma_ramaddr;
@@ -196,7 +200,7 @@ u32 ReadReg(u32 addr)
                 /* SP_DMA_WRLEN and SP_DMA_RDLEN both always returns the same data on read, relative to
                 the current transfer, irrespective on the direction of the transfer. */
                 return ~7 & [&] {
-                    if (dma_in_progress) {
+                    if (sp.status.dma_busy) {
                         return in_progress_dma_type == DmaType::RdToSp ? sp.dma_rdlen : sp.dma_wrlen;
                     } else {
                         return sp.dma_rdlen;
@@ -205,7 +209,7 @@ u32 ReadReg(u32 addr)
 
             case DmaWrlen:
                 return ~7 & [&] {
-                    if (dma_in_progress) {
+                    if (sp.status.dma_busy) {
                         return in_progress_dma_type == DmaType::RdToSp ? sp.dma_rdlen : sp.dma_wrlen;
                     } else {
                         return sp.dma_wrlen;
@@ -270,28 +274,26 @@ void WriteReg(u32 addr, u32 data)
         case DmaRamaddr: sp.dma_ramaddr = data &= 0x03FF'FFF8; break;
 
         case DmaRdlen:
-            if (dma_in_progress) {
+            if (sp.status.dma_busy) {
                 buffered_dma_rdlen = data & 0xFF8F'FFFF;
-                dma_is_pending = true;
+                sp.status.dma_full = 1;
                 sp.dma_full |= 1;
-                sp.status.dma_full = 1; /* Mirrors dma_full.0 */
-                init_pending_dma_fun_ptr = InitDMA<DmaType::RdToSp>;
+                pending_dma_type = DmaType::RdToSp;
             } else {
                 sp.dma_rdlen = data & 0xFF8F'FFFF;
-                InitDMA<DmaType::RdToSp>();
+                InitDma<DmaType::RdToSp>();
             }
             break;
 
         case DmaWrlen:
-            if (dma_in_progress) {
+            if (sp.status.dma_busy) {
                 buffered_dma_wrlen = data & 0xFF8F'FFFF;
-                dma_is_pending = true;
+                sp.status.dma_full = 1;
                 sp.dma_full |= 1;
-                sp.status.dma_full = 1; /* Mirrors dma_full.0 */
-                init_pending_dma_fun_ptr = InitDMA<DmaType::SpToRd>;
+                pending_dma_type = DmaType::SpToRd;
             } else {
                 sp.dma_wrlen = data & 0xFF8F'FFFF;
-                InitDMA<DmaType::SpToRd>();
+                InitDma<DmaType::SpToRd>();
             }
             break;
 
@@ -351,8 +353,7 @@ void WriteReg(u32 addr, u32 data)
             break;
         }
 
-        case DmaFull: /* read-only */ break;
-
+        case DmaFull:
         case DmaBusy: /* read-only */ break;
 
         case Semaphore:
