2e1472ea14bd3c9be601cddafae87d92554a6fc9
Christoffer Ekgrim <christoffer.ekgrim98@hotmail.com>
2023-04-16 21:05:15 +0200

    [N64] SI DMA refactor

--- src/n64/interface/si.cpp
+++ src/n64/interface/si.cpp
@@ -29,93 +29,61 @@ enum Register {
 };
 
 struct {
-    s32 dram_addr, pif_addr_rd64b, pif_addr_wr4b, dummy0, pif_addr_wr64b, pif_addr_rd4b, status, dummy1;
+    u32 dram_addr, pif_addr_rd64b, pif_addr_wr4b, dummy0, pif_addr_wr64b, pif_addr_rd4b;
+    struct {
+        u32 dma_busy     : 1;
+        u32 io_busy      : 1;
+        u32 read_pending : 1;
+        u32 dma_error    : 1;
+        u32              : 8;
+        u32 interrupt    : 1;
+        u32              : 19;
+    } status;
+    u32 dummy1;
 } static si;
-
-static size_t dma_len;
-static s32* pif_addr_reg_last_dma;
 
 template<DmaType> static void InitDma();
 static void OnDmaFinish();
 constexpr std::string_view RegOffsetToStr(u32 reg_offset);
 
-void ClearStatusFlag(StatusFlag status_flag)
-{
-    si.status &= ~std::to_underlying(status_flag);
-}
-
 template<DmaType type> void InitDma()
 {
-    SetStatusFlag(StatusFlag::DmaBusy);
-    u32 pif_addr;
-    size_t bytes_until_pif_end;
+    si.status.dma_busy = 1;
     if constexpr (type == DmaType::PifToRdram) {
-        pif_addr = si.pif_addr_rd64b;
-        pif_addr_reg_last_dma = &si.pif_addr_rd64b;
-    } else { /* RDRAM to PIF */
-        pif_addr = si.pif_addr_wr64b;
-        pif_addr_reg_last_dma = &si.pif_addr_wr64b;
-    }
-    bytes_until_pif_end = pif::GetNumberOfBytesUntilMemoryEnd(pif_addr);
-    u8* rdram_ptr = rdram::GetPointerToMemory(si.dram_addr);
-    size_t bytes_until_rdram_end = rdram::GetNumberOfBytesUntilMemoryEnd(si.dram_addr);
-    static constexpr size_t max_dma_len = 64;
-    dma_len = std::min(max_dma_len, std::min(bytes_until_rdram_end, bytes_until_pif_end));
-    if constexpr (type == DmaType::PifToRdram) {
-        u8* pif_ptr = pif::GetPointerToMemory(pif_addr);
-        for (size_t i = 0; i < dma_len; i += 4) {
-            s32 val;
-            std::memcpy(&val, pif_ptr + i, 4);
-            val = std::byteswap(val); // PIF BE, RDRAM LE
-            std::memcpy(rdram_ptr + i, &val, 4);
-        }
         if constexpr (log_dma) {
-            log(std::format("DMA: from PIF ${:X} to RDRAM ${:X}: ${:X} bytes", pif_addr, si.dram_addr, dma_len));
+            log(std::format("DMA: from PIF ${:X} to RDRAM ${:X}: ${:X} bytes", si.pif_addr_rd64b, si.dram_addr, 64));
         }
+        for (int i = 0; i < 16; ++i) {
+            rdram::Write<4>(si.dram_addr, pif::ReadMemory<s32>(si.pif_addr_rd64b));
+            si.dram_addr += 4;
+            si.pif_addr_rd64b += 4;
+        }
+
     } else { /* RDRAM to PIF */
-        size_t num_bytes_in_rom_area = pif::GetNumberOfBytesUntilRamStart(pif_addr);
-        if (num_bytes_in_rom_area < dma_len) {
-            pif_addr += num_bytes_in_rom_area;
-            rdram_ptr += num_bytes_in_rom_area;
-            dma_len -= num_bytes_in_rom_area;
-            for (size_t i = 0; i < dma_len; i += 4) {
-                s32 val;
-                std::memcpy(&val, rdram_ptr + i, 4);
-                pif::WriteMemory<4>(pif_addr + i, val); // PIF BE, RDRAM LE, but val byteswapped in pif::WriteMemory
-            }
-            if constexpr (log_dma) {
-                log(std::format("DMA: from RDRAM ${:X} to PIF ${:X}: ${:X} bytes",
-                  si.dram_addr,
-                  pif_addr,
-                  dma_len - num_bytes_in_rom_area));
-            }
-        } else {
-            log(std::format(
-              "Attempted from RDRAM ${:X} to PIF ${:X}, but the target PIF memory area was entirely in the ROM region",
-              si.dram_addr,
-              pif_addr));
-            OnDmaFinish();
-            return;
+        if constexpr (log_dma) {
+            log(std::format("DMA: from RDRAM ${:X} to PIF ${:X}: ${:X} bytes", si.dram_addr, si.pif_addr_wr64b, 64));
+        }
+        for (int i = 0; i < 16; ++i) {
+            pif::WriteMemory<4>(si.pif_addr_wr64b, rdram::Read<s32>(si.dram_addr));
+            si.dram_addr += 4;
+            si.pif_addr_wr64b += 4;
         }
     }
-
     static constexpr auto cycles_per_byte_dma = 18;
-    auto cycles_until_finish = dma_len * cycles_per_byte_dma;
+    auto cycles_until_finish = 64 * cycles_per_byte_dma;
     scheduler::AddEvent(scheduler::EventType::SiDmaFinish, cycles_until_finish, OnDmaFinish);
 }
 
 void Initialize()
 {
-    std::memset(&si, 0, sizeof(si));
+    si = {};
 }
 
 void OnDmaFinish()
 {
-    SetStatusFlag(StatusFlag::Interrupt);
-    ClearStatusFlag(StatusFlag::DmaBusy);
+    si.status.interrupt = 1;
+    si.status.dma_busy = 0;
     mi::RaiseInterrupt(mi::InterruptType::SI);
-    si.dram_addr = (si.dram_addr + dma_len) & 0xFF'FFFF;
-    *pif_addr_reg_last_dma = (*pif_addr_reg_last_dma + dma_len) & 0x7FC;
 }
 
 u32 ReadReg(u32 addr)
@@ -141,11 +109,6 @@ constexpr std::string_view RegOffsetToStr(u32 reg_offset)
     case Status: return "SI_STATUS";
     default: return "UNKNOWN";
     }
-}
-
-void SetStatusFlag(StatusFlag status_flag)
-{
-    si.status |= std::to_underlying(status_flag);
 }
 
 void WriteReg(u32 addr, u32 data)
@@ -184,9 +147,8 @@ void WriteReg(u32 addr, u32 data)
     case Register::Status:
         /* Writing any value to si.STATUS clears bit 12 (SI Interrupt flag), not only here,
            but also in the RCP Interrupt Cause register and in MI. */
-        ClearStatusFlag(StatusFlag::Interrupt);
+        si.status.interrupt = 0;
         mi::ClearInterrupt(mi::InterruptType::SI);
-        // TODO: RCP flag
         break;
 
     default: log_warn(std::format("Unexpected write made to SI register at address ${:08X}", addr));
